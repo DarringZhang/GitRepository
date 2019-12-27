c987795d7299cd3874c0f75e4bb796331d256db6
DarringZhang <1002970363@qq.com>
2019-12-28 00:06:55 +0800

    regression 多因素预测财政总收入

---
+++ Experiments/DataMining/regression/prediction.py
@@ -0,0 +1,58 @@
+# encoding=utf-8
+#开始建立模型进行预测
+
+import xlrd
+import xlwt
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+inputfile='data4.csv'
+data= pd.read_csv(inputfile)
+
+from sklearn.linear_model import Lasso
+# LASSO回归的特点是在拟合广义线性模型的同时进行变量筛选和复杂度调整。 因此，不论目标因变量是连续的，还是二元或者多元离散的，
+#都可以用LASSO回归建模然后预测。 这里的变量筛选是指不把所有的变量都放入模型中进行拟合，而是有选择的把变量放入模型从而得到更好的性能参数。
+model = Lasso()
+model.fit(data.iloc[:,0:13], data['y'])
+q=model.coef_   #各特征的系数
+q=pd.DataFrame(q,index=['x1','x2','x3','x4','x5','x6','x7','x8','x9','x10','x11','x12','x13']).T
+np.round(q,4).to_csv('dataLasso.csv')
+#得出 可以踢掉x12
+
+
+
+
+#地方财政收入神经网络预测模型
+inputfile='data.csv'
+outputfile='revenue.xls'
+modelfile='1-net.model'
+data=pd.read_csv(inputfile)
+print(data)
+feature=['x1','x2','x3','x4','x5','x6','x7','x8','x9','x10','x11','x13']#特征所在列
+data_train=data.loc[range(0,19)].copy() #取2015年前的建模数据
+data_mean=data_train.mean()
+data_std=data_train.std()
+data_train=(data_train-data_mean)/data_std#数据标准化
+x_train=data_train[feature].as_matrix()#特征数据
+y_train=data_train['y'].as_matrix()#标签数据
+from keras.models import Sequential
+from keras.layers.core import Dense,Activation
+
+tf.reset_default_graph()
+model=Sequential()#建立模型
+model.add(Dense(input_dim=12,output_dim=12))
+model.add(Activation('relu'))#激活函数
+model.add(Dense(input_dim=12,output_dim=1))
+model.compile(loss='mean_squared_error',optimizer='adam')#编译模型，目标函数是均方差
+model.fit(x_train,y_train,nb_epoch=10000,batch_size=2)#训练模型
+model.save_weights(modelfile)#保存模型
+#预测并还原结果
+x=((data[feature]-data_mean[feature])/data_std[feature]).as_matrix()
+data[u'y_pred'] = model.predict(x) * data_std['y'] + data_mean['y']
+data.to_excel(outputfile)
+#画出预测图
+import matplotlib.pyplot as plt
+p=data[['y','y_pred']].plot(subplots=True,style=['b-o','r-*'])
+plt.show()
+
+
---
+++ Experiments/DataMining/regression/readme.txt
@@ -0,0 +1,17 @@
+下面对各个文件进行介绍：
+data.csv：  测试集
+data4.csv:  训练集
+DataPredeal.py ： 对数据进行描述型分析，分析输出自变量的最值，均值和方差（analyse.csv）
+				 变量之间的相关性Pearson系数矩阵（pearson.csv）
+
+prediction.py ：通过LASSO回归建模，数据预处理，选取相关性高的因素（dataLasso.csv）
+		神经网络预测训练模型并进行预测处结果（revenue.xls）
+
+GM11:	灰色预测通过鉴别系统因素之间发展趋势的相异程度，即进行关联分析，
+	并对原始数据进行生成处理来寻找系统变动的规律，
+	生成有较强规律性的数据序列，然后建立相应的微分方程模型，从而预测事物未来发展趋势的状况
+	这里用来预测下一年的自变量x,  题目没有涉及，暂且没用到
+
+参考文献：代码和思想  https://blog.csdn.net/y1535766478/article/details/77992544
+	绘图：https://blog.csdn.net/sinat_17588957/article/details/90600420
+	灰色预测：https://blog.csdn.net/u012063773/article/details/79324324
